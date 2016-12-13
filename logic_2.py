bd1a9cd8aa6dcc7a16a14f9da671c3356bee0fad
Jason Brown <ahnakel@gmail.com>
2016-12-13 14:05:25 -0500

    adding latest solutions

---
+++ logic_2.py
@@ -0,0 +1,7 @@
+#Exercise1
+#Figuring out how to do this in one line
+
+#Exercise2
+lone_sum = lambda a, b, c: [[a,[b,[c,0][c==a or c==b]][b==a or b==c]][a==b or a==c],a+b+c][(a != b != c) and a != c]
+
+#Exercise3
---
+++ string_2.py
@@ -0,0 +1,16 @@
+#Exercise1
+double_char = lambda str: "".join([x*2 for x in str])
+
+#Exercise2
+count_hi = lambda str: str.count('hi')
+
+#Exercise3
+cat_dog = lambda str: str.count('cat') == str.count('dog')
+
+#Exercise4
+#Coming back to this later
+
+#Exercise5
+end_other = lambda a, b: a.lower()[-len(b):] == b.lower() if len(a) >= len(b) else b.lower()[-len(a):] == a.lower()
+
+#Exercise6
