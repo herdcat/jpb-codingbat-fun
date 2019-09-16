9c8dc41f0d76f24556852885e81b0f477b1b669d
lunuslovesgreat <ahnakel@gmail.com>
2019-09-15 22:27:20 -0700

    added another round of tests

--- test_exercises.py
+++ test_exercises.py
@@ -40,3 +40,47 @@ class TestMyTestCases(unittest.TestCase):
 
     def test_make_pi(self):
         self.assertEqual(self.cb_solutions().make_pi(),[3, 1, 4])
+
+    def test_common_end(self):
+        tests = {
+            'test0' : ([0, 1, 2], [0, 1, 2], True),
+            'test1' : ([0, 1, 2], [0, 1, 3], True),
+            'test2' : ([0, 1, 2], [1, 1, 2], True),
+            'test3' : ([1, 1, 2], [0, 1, 0], False),
+            'test4' : ([1, 2, 3], [2, 3, 4], False),
+            'test5' : (['a', 'b, c'], ['c', 'b', 'a'], False)
+        }
+        for test in tests.values():
+            self.assertEqual(self.cb_solutions().common_end(test[0], test[1]),test[2])
+
+    def test_sum3(self):
+        tests = {
+            'test0' : ([0, 1, 2], 3),
+            'test1' : ([1, 2, 3], 6),
+            'test2' : ([2, 3, 4], 9),
+            'test3' : ([3, 4, 5], 12),
+            'test4' : ([4, 5, 6], 15),
+            'test5' : ([5, 6, 7], 18),
+            'test6' : ([6, 7, 8], 21),
+            'test7' : ([7, 8, 9], 24),
+            'test8' : ([8, 9, 10], 27),
+            'test9' : ([9, 10, 11], 30)
+        }
+        for test in tests.values():
+            self.assertEqual(self.cb_solutions().sum3(test[0]),test[1])
+
+    def test_rotate_left3(self):
+        tests = {
+            'test0' : ([0, 1, 2], [1, 2, 0]),
+            'test1' : ([1, 2, 3], [2, 3, 1]),
+            'test2' : ([2, 3, 4], [3, 4, 2]),
+            'test3' : ([3, 4, 5], [4, 5, 3]),
+            'test4' : ([4, 5, 6], [5, 6, 4]),
+            'test5' : ([5, 6, 7], [6, 7, 5]),
+            'test6' : ([6, 7, 8], [7, 8, 6]),
+            'test7' : ([7, 8, 9], [8, 9, 7]),
+            'test8' : ([8, 9, 10], [9, 10, 8]),
+            'test9' : ([9, 10, 11], [10, 11, 9])
+        }
+        for test in tests.values():
+            self.assertEqual(self.cb_solutions().rotate_left3(test[0]),test[1])
